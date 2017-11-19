068ebc16d4e3097d5e69d037093c4202a3c1c700
andrewt <andrewt@ispras.ru>
2017-11-20 01:35:04 +0400

    Orders test
    Signed-off-by: andrewt <andrewt@ispras.ru>

---
+++ seminars/seminar06/examples/exampleGUI/src/test/java/com/company/orders/OrdersTestNGTest.java
@@ -0,0 +1,45 @@
+package com.company.orders;
+
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.edt.GuiQuery;
+import org.assertj.swing.fixture.FrameFixture;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OrdersTestNGTest {
+    private FrameFixture window;
+
+    @BeforeClass
+    public static void setUpOnce() {
+        FailOnThreadViolationRepaintManager.install();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        final JfrmMain frame = GuiActionRunner.execute(new GuiQuery<JfrmMain>(){
+            @Override
+            protected JfrmMain executeInEDT() throws Throwable {
+                return new JfrmMain();
+            }
+        });
+
+        window = new FrameFixture(frame);
+        window.show(); // shows the frame to test
+    }
+
+    @Test
+    public void testOrders() {
+       window.menuItemWithPath("File", "New").click();
+       window.menuItemWithPath("Orders", "New order...").click();
+       window.dialog().button(JButtonMatcher.withText("Ok"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        window.cleanUp();
+    }
+}
