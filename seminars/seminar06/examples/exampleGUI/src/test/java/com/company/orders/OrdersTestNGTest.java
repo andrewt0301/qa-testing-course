324212c1df93280cc552c6b2e7f231e3df69783b
andrewt <andrewt@ispras.ru>
2017-11-20 10:44:08 +0400

    Working on Task
    Signed-off-by: andrewt <andrewt@ispras.ru>

--- seminars/seminar06/examples/exampleGUI/src/test/java/com/company/orders/OrdersTestNGTest.java
+++ seminars/seminar06/examples/exampleGUI/src/test/java/com/company/orders/OrdersTestNGTest.java
@@ -4,6 +4,7 @@ import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.edt.GuiQuery;
+import org.assertj.swing.fixture.DialogFixture;
 import org.assertj.swing.fixture.FrameFixture;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -35,7 +36,12 @@ public class OrdersTestNGTest {
     public void testOrders() {
        window.menuItemWithPath("File", "New").click();
        window.menuItemWithPath("Orders", "New order...").click();
-       window.dialog().button(JButtonMatcher.withText("Ok"));
+
+       final DialogFixture dialog = window.dialog();
+       dialog.comboBox("jcbProduct").selectItem("FamilyAlbum");
+       dialog.spinner("jspQuantity").enterText("10");
+       dialog.textBox("jtfName").enterText("John Doe");
+       dialog.button(JButtonMatcher.withText("Ok"));
     }
 
     @AfterMethod
