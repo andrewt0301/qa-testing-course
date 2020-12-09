57eb6141cc1122cbede44add0a891cc4ac945345
Andrei Tatarnikov <andrewt0301@gmail.com>
2020-12-09 10:17:50 +0300

    Seminar12 updated
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- seminars/seminar12/examples/exampleSelenium/src/main/java/ru/hse/Main.java
+++ seminars/seminar12/examples/exampleSelenium/src/main/java/ru/hse/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package ru.hse;
 
 import java.nio.file.Paths;
 
--- seminars/seminar12/examples/exampleSelenium/src/test/java/ru/hse/BasicSeleniumTest.java
+++ seminars/seminar12/examples/exampleSelenium/src/test/java/ru/hse/BasicSeleniumTest.java
@@ -1,89 +1,94 @@
-package com.company;
+package ru.hse;
+
+import org.junit.Ignore;
+import org.junit.Test;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
-import java.nio.file.Paths;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasicSeleniumTestNGTest {
+import java.nio.file.Paths;
+
+public class BasicSeleniumTest {
 
   /**
-   * Sets up the Chrome driver.
+   * Runs a simple test in Safari.
+   * <p>
+   * Marked as {@code @Ignore} because there is no information on the environment.
+   * If you are using Safari, just run it manually or remove {@code @Ignore}.
    */
-  public static void initChromeDriver() {
+  @Ignore
+  @Test
+  public void testSafari() {
+    // In MacOS, WebDriver for Safari is already installed in this folder: '/usr/bin/safaridriver'.
+    // No additional setup is required.
+    simpleTest(new SafariDriver());
+  }
+
+  /**
+   * Runs a simple test in Chrome.
+   * <p>
+   * Marked as {@code @Ignore} because there is no information on the environment.
+   * If you are using Chrome, just run it manually or remove {@code @Ignore}.
+   */
+  @Ignore
+  @Test
+  public void testChrome() {
     // Proper version of chromedriver can be downloaded from here:
     // https://chromedriver.chromium.org
     final String pathToDriver = Paths.get("chromedriver").toAbsolutePath().toString();
     System.setProperty("webdriver.chrome.driver", pathToDriver);
+    // Now we run the test.
+    simpleTest(new ChromeDriver());
   }
 
   /**
-   * Sets up the Firefox driver.
+   * Runs a simple test in Firefox.
+   * <p>
+   * Marked as {@code @Ignore} because there is no information on the environment.
+   * If you are using Firefox, just run it manually or remove {@code @Ignore}.
    */
-  public static void initFirefoxDriver() {
+  @Ignore
+  @Test
+  public void testFirefox() {
     // Proper version of geckodriver can be downloaded from here:
     // https://github.com/mozilla/geckodriver/releases
     final String pathToGeckoDriver = Paths.get("geckodriver").toAbsolutePath().toString();
     System.setProperty("webdriver.gecko.driver", pathToGeckoDriver);
+    // Now we run the test.
+    simpleTest(new FirefoxDriver());
   }
 
-  @BeforeClass
-  public static void setUpClass() throws Exception {
-    initChromeDriver();
-  }
-
-  @Test
-  public void testMain() throws Exception {
-    testGeneric(new SafariDriver());
-  }
-
-  @Test
-  public void testMain1() throws Exception {
-    testGeneric(new ChromeDriver());
-  }
-
-  public void testGeneric(final WebDriver driver) throws Exception {
-    // Create a new instance of the Firefox driver
-    // Notice that the remainder of the code relies on the interface,
-    // not the implementation.
-
-
+  /**
+   * Simple web test that can be run in any browser.
+   *
+   * @param driver Web drive for any browser type.
+   */
+  private void simpleTest(final WebDriver driver) {
     // And now use this to visit Google
     driver.get("http://www.google.com");
     // Alternatively the same thing can be done like this
     // driver.navigate().to("http://www.google.com");
-
     // Find the text input element by its name
     final WebElement element = driver.findElement(By.name("q"));
-
     // Enter something to search for
     element.sendKeys("Cheese!");
-
     // Now submit the form. WebDriver will find the form for us from the element
     element.submit();
-
     // Check the title of the page
     System.out.println("Page title is: " + driver.getTitle());
-
     // Google's search is rendered dynamically with JavaScript.
     // Wait for the page to load, timeout after 10 seconds
-    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-      public Boolean apply(WebDriver d) {
-        return d.getTitle().toLowerCase().startsWith("cheese!");
-      }
-    });
-
+    (new WebDriverWait(driver, 10)).until(
+        d -> d.getTitle().toLowerCase().startsWith("cheese!")
+    );
     // Should see: "cheese! - Google Search"
     System.out.println("Page title is: " + driver.getTitle());
-
     //Close the browser
     driver.quit();
   }
